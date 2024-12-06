1b9a90ac8889e84b321852af5830f41421868828
hamza-ridhwan <hamzathehamzah@gmail.com>
2024-12-06 22:31:56 +0300

    Completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+       print([])
+    elif length == 1:
+       print([0])
+    else:
+        my_two_numbers = [0,1]
+        for _ in range(2, length):
+         my_two_numbers.append(my_two_numbers[-1] + my_two_numbers[-2])
+        
+        print(my_two_numbers)
+
+print_fibonacci(9)
